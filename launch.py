5868fba013b75324f63b33ffafb03901d965b7e6
Ps3291 <ps32911@gmail.com>
2023-06-21 12:19:59 +0200

    Update launch.py

--- launch.py
+++ launch.py
@@ -1,7 +1,3 @@
+# This is simple program to show how print statement works
 
-helloworld = Flask(__name__)
-@helloworld.route("/")
-def run():
-    return "{\"message\":\"Hello Worlds\"}"
-if __name__ == "__main__":
-    helloworld.run(host="0.0.0.0", port=int("5000"), debug=True)
+print('Hello Python World')
